4f4d5ebcf7514fc8f21ac8168899f20aa5590750
amitgupta7 <amit864@gmail.com>
2023-06-17 00:56:04 +0530

    working infra provisioning from vm_map variable

--- main.tf
+++ main.tf
@@ -1,14 +1,108 @@
 terraform {
   required_providers {
-    az = {
-        source = "hashicorp/azurerm"
-        version = "latest"
+    azurerm = {
+      source = "hashicorp/azurerm"
     }
   }
 }
 
-provider "az" {
-  subscription_id = var.az_subscription_id
+provider "azurerm" {
+  features {}
+  subscription_id            = var.az_subscription_id
   skip_provider_registration = true
 }
 
+resource "azurerm_virtual_network" "vnet" {
+  name                = "${var.az_name_prefix}_pod-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = var.location
+  resource_group_name = var.az_resource_group
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "${var.az_name_prefix}_pod-subnet"
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  resource_group_name  = var.az_resource_group
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_security_group" "pod_sg" {
+  name                = "${var.az_name_prefix}_pods-sg"
+  location            = var.location
+  resource_group_name = var.az_resource_group
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 101
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_public_ip" "pod_ip" {
+  for_each            = var.vm_map
+  name                = "${var.az_name_prefix}_${each.key}_ip"
+  location            = var.location
+  resource_group_name = var.az_resource_group
+  allocation_method   = "Dynamic"
+  domain_name_label   = "${var.az_name_prefix}-${each.key}"
+}
+
+resource "azurerm_network_interface" "pod_nic" {
+  for_each            = var.vm_map
+  name                = "${var.az_name_prefix}_${each.key}_nic"
+  location            = var.location
+  resource_group_name = var.az_resource_group
+  ip_configuration {
+    name                          = "${var.az_name_prefix}_${each.key}_ipconfig"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Static"
+    private_ip_address            = each.value["private_ip_address"]
+    public_ip_address_id          = azurerm_public_ip.pod_ip[each.key].id
+  }
+}
+
+resource "azurerm_network_interface_security_group_association" "nsg" {
+  for_each                  = var.vm_map
+  network_interface_id      = azurerm_network_interface.pod_nic[each.key].id
+  network_security_group_id = azurerm_network_security_group.pod_sg.id
+}
+
+resource "azurerm_linux_virtual_machine" "podvms" {
+  for_each              = var.vm_map
+  name                  = "${var.az_name_prefix}-${each.key}-vm"
+  network_interface_ids = [azurerm_network_interface.pod_nic[each.key].id]
+  //variables
+  location            = var.location
+  resource_group_name = var.az_resource_group
+  size                = var.vm_size
+  os_disk {
+    name                 = "${var.az_name_prefix}-${each.key}-os-disk"
+    disk_size_gb         = var.os_disk_size_in_gb
+    caching              = "ReadWrite"
+    storage_account_type = "StandardSSD_LRS"
+  }
+  source_image_reference {
+    publisher = var.os_publisher
+    offer     = var.os_offer
+    sku       = var.os_sku
+    version   = var.os_version
+  }
+  admin_username                  = var.azuser
+  admin_password                  = var.azpwd
+  disable_password_authentication = false
+
+}
+
+output "hostnames" {
+  value = values(azurerm_public_ip.pod_ip).*.fqdn
+}
+
+output "ssh_credentials" {
+  value = "${var.azuser}/${var.azpwd}"
+}
--- var.tf
+++ var.tf
@@ -1,9 +1,64 @@
 variable "az_subscription_id" {
-    description = "azure subscription id"
-    type = string
+  description = "azure subscription id"
+  type        = string
+}
+
+variable "location" {
+  default = "westus2"
+}
+
+variable "azuser" {
+  default = "azuser"
+}
+
+variable "azpwd" {
+  description = "azure vm password, common for all machines"
+}
+
+variable "vm_size" {
+  default = "Standard_D32s_v3"
+}
+
+variable "os_disk_size_in_gb" {
+  default = 1024
+}
+
+
+variable "os_publisher" {
+  default = "Canonical"
+}
+
+variable "os_offer" {
+  default = "UbuntuServer"
+}
+
+variable "os_sku" {
+  default = "18.04-LTS"
+}
+
+variable "os_version" {
+  default = "latest"
 }
 
 variable "az_resource_group" {
-    description = "resource group to create these resources"
-    default = "securiti-sepm"  
+  description = "resource group name to create these resources"
+}
+
+variable "az_name_prefix" {
+  description = "prefix to add to resource names"
+  default     = "azure-tf-vms"
+}
+
+variable "vm_map" {
+  type = map(object({
+    private_ip_address = string
+  }))
+  default = {
+    "pod1" = {
+      private_ip_address = "10.0.2.21"
+    }
+    "pod2" = {
+      private_ip_address = "10.0.2.22"
+    }
+  }
 }
