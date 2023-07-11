a1449315d2af523e8eae9cb5bbd9d33a6ae1227a
amitgupta7 <amit864@gmail.com>
2023-07-11 15:16:37 +0530

    added API call to retreve appliance_diagnostics_script_url

--- create_sai_appliance.sh
+++ create_sai_appliance.sh
@@ -20,3 +20,9 @@ curl -s -X 'GET' \
   -H 'X-API-Secret:  '$X_API_Secret \
   -H 'X-API-Key:  '$X_API_Key \
   -H 'X-TIDENT:  '$X_TIDENT | jq 
+curl -s -X 'GET' \
+  'https://app.securiti.ai/core/v1/admin/appliance/download_diagnostics_url' \
+  -H 'accept: application/json' \
+  -H 'X-API-Secret:  '$X_API_Secret \
+  -H 'X-API-Key:  '$X_API_Key \
+  -H 'X-TIDENT:  '$X_TIDENT | jq '{appliance_diagnostics_script_url: .download_diagnostics_url}'
