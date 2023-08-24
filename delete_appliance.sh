e97a1d967139a9a0015ff4d60a934b993a5e154d
amitgupta7 <amit864@gmail.com>
2023-08-24 20:59:57 +0530

    env fix

--- delete_appliance.sh
+++ delete_appliance.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/sh
-set -a && source .env && set +a
+set -a && source `pwd`/.env && set +a
 [ $# -eq 0 ] && { echo "Usage: $0 <sai_appliance_id>"; exit 1; }
 curl -s -X 'DELETE' \
   'https://app.securiti.ai/core/v1/admin/appliance/'$1 \
