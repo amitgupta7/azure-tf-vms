edc0b54a36eb9a2098909c4fc45e6eeba7ead256
amitgupta7 <amit864@gmail.com>
2023-11-22 00:46:20 +0530

    Added colored output to status script.

--- install_status.sh
+++ install_status.sh
@@ -1,18 +1,19 @@
 #!/usr/bin/sh
 set -e
+R='\033[1;31m'
+G='\033[1;32m'
 if [ -f $1 ] 
  then
    echo "Existing Installation Lock File Found: "$1
    echo -n 'Installer Status: ' 
    if [ ! -s $1 ]
     then
-        echo 'In Progress'
+        echo "${G}In Progress"
     else
         state=$(cat $1)
         if [ $state -eq 0 ]
             then
-              echo -n 'Completed on '
-              echo $3
+              echo "${G}Completed on $3"
               if [ $3 = "master" ]
                 then
                  SVC_NAME=k3s
@@ -21,10 +22,11 @@ if [ -f $1 ]
                 else
                  SVC_NAME=k3s-agent
               fi
-                systemctl --no-pager status $SVC_NAME | grep --color=always Active
+                echo "Service status of $SVC_NAME on $3: $(systemctl is-active --quiet $SVC_NAME && echo "${G}Active" || echo "${R}Not-Active")" 
+                systemctl --no-pager status $SVC_NAME | head -5
                 exit 0
             else
-                echo 'Error' 
+                echo "${R}Error"
                 $2 && (echo "clearing lockfile: $1" && rm $1 && echo "cleared lockfile, rerunning installer as nohup (rerun tfaa to tail install log)") || (tail /home/$SUDO_USER/appliance_init.out && echo "[EXPERIMENTAL] Use command to clear lockfile and rerun the install:  tfaa -var=clr_lock=true") 
                 exit 0
         fi   
